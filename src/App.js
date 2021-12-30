bb3edadaae8de9389ed96ae6e69901c80f4c5ea3
Matheus <matheuso.silva95@gmail.com>
2021-12-30 12:22:59 -0300

    estrutura da home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { BrowserRouter as Router, Routes, Switch, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+
+import Container from './components/layout/Container'
+
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
+    
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
